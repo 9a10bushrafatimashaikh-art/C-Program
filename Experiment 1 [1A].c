42d728cd47bc5492c0791e91a127c7dac5a5931b
9a10bushrafatimashaikh-art <9a10bushrafatimashaikh@gmail.com>
2025-11-21 23:29:30 +0530

    Add files via upload

---
+++ Experiment 1 [1A].c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Your name is Bushra Shaikh\n");
+    printf("Age: 19 yrs\n");
+    printf("Class: AI & DS\n");
+    printf("Division: A\n");
+    printf("UIN: 251A019\n");
+
+    return 0;
+}
