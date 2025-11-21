22b0873e5703b1fbdae1f75fcc8b5b2987bb5dab
9a10bushrafatimashaikh-art <9a10bushrafatimashaikh@gmail.com>
2025-11-21 23:33:05 +0530

    Update Experiment 8 [1B].c

--- Experiment 8 [1B].c
+++ Experiment 8 [1B].c
@@ -1,9 +1,3 @@
-/*
-Name: ASHAZ SHAKIR PATEL
-UIN: 251M003
-Class: FE Mechanical Engineering
-Div: F
-*/
 #include <stdio.h>
 int main() {
     char str[100];
@@ -29,3 +23,4 @@ int main() {
 
     return 0;
 }
+
